3829fee50bdbc445a250c2111fdb1f63645d0518
TitamSeptian <dwiputra4456@protonmail.com>
2023-06-11 14:23:58 +0700

    fix: response

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 import tensorflow as tf
 import re
 from fastapi import FastAPI, Response, status
@@ -6,19 +6,20 @@ from pydantic import BaseModel
 import uvicorn
 import numpy as np
 import traceback
-from typing import Union
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import re
 import nltk
 from nltk.corpus import stopwords
+import pandas as pd
 
 nltk.download('stopwords')
 stopwords_uses = stopwords.words('english')
 
-maxlen = 100  # Set the desired max sequence length
+maxlen = 10  # Set the desired max sequence length
 pad_type = 'post'
 trunc_type = 'post'
+
+df_vegan_test = pd.read_csv('df_vegan_test.csv')
 
 def preprocess_title(sentence):
     # Remove trailing spaces
@@ -30,22 +31,33 @@ def preprocess_title(sentence):
     # Remove stopwords
     sentence = ' '.join([word for word in sentence.split() if word not in stopwords_uses])
     return sentence
-
 tokenizer = Tokenizer(oov_token='<OOV>')
-word_index = {}
+
+input_title = df_vegan_test['title']  # Add your input titles here
+processed_titles = [preprocess_title(title) for title in input_title]
+tokenizer.fit_on_texts(processed_titles)
+input_seq = tokenizer.texts_to_sequences(processed_titles)
+
+word_index = tokenizer.word_index
+# print(word_index)
+
+input_padded = pad_sequences(input_seq, padding=pad_type, truncating=trunc_type, maxlen=maxlen)
 
 def preprocess_texts_input(sentences):
-    processed_sentences = []
-    for sentence in sentences:
-        sentence = preprocess_title(sentence)
-        processed_sentences.append(sentence)
+    processed_sentences = [preprocess_title(sentence) for sentence in sentences]
     tokenizer.fit_on_texts(processed_sentences)
     input_seq = tokenizer.texts_to_sequences(processed_sentences)
     input_seq = pad_sequences(input_seq, padding=pad_type, truncating=trunc_type, maxlen=maxlen)
     return input_seq
 
+# sentences = ['This is a sample sentence.', 'Another sample sentence.']
+# processed_sentences = preprocess_texts_input(sentences)
+# print("Processed Sentences:")
+# print(processed_sentences)
+
+
 class ModelInput(BaseModel):
-    sentence: Union[str, List[str]]
+    sentence: str
 
 app = FastAPI()
 
@@ -60,25 +72,23 @@ tokenizer = Tokenizer()
 @app.post("/recommend")
 async def recommendation(request: ModelInput):
     try:
-        sentences = request.sentence
+        sentences = [request.sentence]
         sentences = preprocess_texts_input(sentences)
+        print(sentences)
         prediction = model.predict(sentences)
 
-        # get the output with the highest probability
-        top_indices = np.argsort(prediction[0])[-5:][::-1]
-        print("Top Indices:", top_indices)
+        threshold = 0.5
 
-        word_index_reverse = {index: word for word, index in tokenizer.word_index.items()}
-        print("Word Index Reverse:", word_index_reverse)
-
-        top_recipes = [word_index_reverse[index] for index in top_indices if index in word_index_reverse]
-
-        json_response = {"topRecommendationRecipes": top_recipes}
+        recommendation = []
+        for index, pred in enumerate(prediction[0]):
+            if pred > threshold:
+                recommendation.append(str(df_vegan_test['title'].iloc[index]))
+        top_recommendations = recommendation[:5]
+        json_response = {"topRecommendationRecipes": top_recommendations}
         return json_response
     except Exception as e:
         print(traceback.format_exc())
         return Response(content=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
